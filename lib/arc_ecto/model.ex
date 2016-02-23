228d0c1d2c0bb85c1b31589f45d49d84268ce33f
David Kuhta <davidkuhta@gmail.com>
2016-02-22 20:02:26 -0600

    Removing :invalid in favor of empty map %{}
    See Ecto #1274

--- lib/arc_ecto/model.ex
+++ lib/arc_ecto/model.ex
@@ -17,7 +17,6 @@ defmodule Arc.Ecto.Model do
       end
 
       arc_params = case params do
-        :invalid -> :invalid
         %{} ->
           params
           |> Arc.Ecto.Model.convert_params_to_binary
